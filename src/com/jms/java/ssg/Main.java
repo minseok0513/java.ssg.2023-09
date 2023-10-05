0ac3027b84d3794991cee17c00903b2e0d7cbdbe
jominseok0513 <jominseok0513@gmail.com>
2023-10-05 19:47:47 +0900

    게시물 상세페이지 기능 구현, 리펙토링 중

--- src/com/jms/java/ssg/Main.java
+++ src/com/jms/java/ssg/Main.java
@@ -29,19 +29,39 @@ public class Main {
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
-				Article article = new Article(id,title,body);
+				Article article = new Article(id, title, body);
 				articles.add(article);
 				System.out.printf("%d번 글이 생성되었습니다.\n", id);
 			} else if (command.equals("article list")) {
-				if(articles.size()==0) {
+				if (articles.size() == 0) {
 					System.out.println("게시물이 없습니다.");
 					continue;
 				}
 				System.out.println("번호, 제목");
-				for(int i =articles.size(); i>=0; i--) {
-					Article article=articles.get(i);
-					System.out.printf("%d, %s\n",article.id,article.title);
+				for (int i = articles.size(); i >= 0; i--) {
+					Article article = articles.get(i);
+					System.out.printf("%d, %s\n", article.id, article.title);
 				}
+			} else if (command.startsWith("article detail ")) {
+				String[] commandBits = command.split(" ");
+				int id = Integer.parseInt(commandBits[2]);
+				Article foundArticle = null;
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+					if (article.id == id) {
+						foundArticle = article;
+						return;
+					}
+				}
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.", 1);
+					continue;
+				}
+				System.out.printf("번호 : %d\n", foundArticle.id);
+				System.out.printf("날짜 : %s\n", id);
+				System.out.printf("제목 : %s\n", foundArticle.title);
+				System.out.printf("내용 : %s\n", foundArticle.body);
+
 			} else {
 				System.out.printf("%s(은)는 존재하지 않는 명령어 입니다.\n", command);
 			}
@@ -56,10 +76,10 @@ class Article {
 	int id;
 	String title;
 	String body;
-	
-	public Article(int id,String title, String body) {
-		this.id=id;
-		this.title=title;
-		this.body=body;
+
+	public Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
 	}
 }
